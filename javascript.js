3abc9bcf9dca9d395919f1ff8663c65d554ef12f
pantro <francoguns16@gmail.com>
2020-03-17 10:39:17 -0500

    haciendo calificaciones

---
+++ javascript.js
@@ -0,0 +1,23 @@
+let calificacion =17; 
+
+if(calificacion ==20){
+  console.log("Excelente");
+}
+   else if(calificacion > 18){
+   console-log("Muy bien");
+}
+   else if(calificacion >16){
+   console.log("Vas por buen camino");
+}
+   else if(calificacion >14){
+   console.log("Puedes mejorar");
+}
+   else if(calificacion >11){
+     console.log("Por poco")
+}
+   else if(calificacion >9){
+   console.log("Reprobaste")
+}
+else{
+  console.log("No estudiaste, Reprobaste");
+}
