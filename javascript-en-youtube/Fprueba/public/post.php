a1d5f73fd245160391b71366369167c74e5b1fa6
pantro <francoguns16@gmail.com>
2020-03-21 13:51:20 -0500

    Como crear Tablas y principios de como crear un login

---
+++ javascript-en-youtube/Fprueba/public/post.php
@@ -0,0 +1,10 @@
+<?php
+echo "hola";
+$usuario= $_POST['usuario'];
+$pass = $_POST['pass'];
+if($usuario ==='' || $pass=== ''){
+    echo json_encode('Llena todos los campos');
+}else{
+    echo json_encode('Correcto: <br>Usuario:'.$usuario.' <br>Pass:'.$pass);
+}
+
